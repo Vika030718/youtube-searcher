896185a915d2a4bd2dfabb9c9f626369c67434ce
Viktoria Matveeva <vika030718@gmail.com>
2018-08-20 18:51:09 +0300

    fix hash

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask import Flask
 from config import basedir
-from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-bcrypt = Bcrypt(app)
-
 
 from app import views, models
